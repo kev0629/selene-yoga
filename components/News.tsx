21d9f4e0f2ab4f5292027c1d92bb653b4a8b11f4
Kevin Saffioti <kevin.saffioti@gmail.com>
2022-12-02 17:47:25 +0100

    Update News.tsx

--- components/News.tsx
+++ components/News.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 
+
 type Props = {}
+
+const Card = () => {
+  return(
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+          <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+      </a>
+      <div className="p-5">
+          <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Read more
+              <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+      </div>
+    </div>
+  )
+}
+
 
 const News = (props: Props) => {
   return (
-    <div>News</div>
+    
+    <section>
+      <div className='flex'>
+        <div className="w-2/12 flex items-center">
+          <div className='w-full text-right'>
+            <button className='p-3 rounded-full bg-white border border-grey-700 shadow-lg'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
+            </button>
+          </div>
+        </div>
+        <div  id='sliderContainer' className="w-full" >
+          <ul id='slider' className='flex w-full boder border-red-500'>
+            <li>
+              <Card/>
+            </li>
+          </ul>
+        </div>
+        <div className="w-2/12 flex items-center" >
+          <div className='w-full'> 
+            <button className='p-3 rounded-full bg-white border border-grey-700 shadow-lg'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </button>
+          </div> 
+        </div>
+      </div>
+    </section>
+
   )
 }
 
